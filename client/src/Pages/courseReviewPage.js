532478b9278d524b9169ee200af3967b776a53f4
尹程 <yc@yinchengdeMacBook-Pro.local>
2018-09-14 20:11:21 +1000

    ReviewStar

--- client/src/Pages/courseReviewPage.js
+++ client/src/Pages/courseReviewPage.js
@@ -1,10 +1,209 @@
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet'; 
+import '../css/style.css';
+import '../css/bootstrap.min.css';
+import '../css/font-awesome.min.css';
+
+import NavigationBar from '../components/NavigationBar';
+
+
+
 class CourseReviewPage extends Component {
-    
   render() {
     return (
-      <div>
-        <p>This is CourseReviewPage!</p>
+      <div>    
+        <div className = "Sbody">
+        {/* <!-- Contact --> */}
+          <div id = "contact" className = "section">
+            <NavigationBar/>
+            {/* <!-- container --> */}
+            <div className = "container">
+                {/* <!-- row --> */}
+                <div className = "row">
+                    {/* <!-- contact information --> */}
+                    <h1>Course Review</h1>
+
+                    
+                    <form action = "/" method = "POST" className = "course_review">
+                        <h2>Semenster 1</h2>
+                        <table width="100%" class="zebra review_table">
+                            <tr>
+                                <td>Course ID</td>
+                                <td>Course Details</td>
+                                <td>Course Review</td>
+                            </tr>
+                            <tr>
+                                <td width="10%" id="CourseID">CourseID</td>
+                                <td width="70%" id="CourseDitails">CourseDitails</td>
+                                <td width="20%" id="Star">
+                                    <div class="review">
+                                        <span class="star">
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                        </span>
+                                        <span class="star-txt"></span>
+                                    </div>
+                                    
+                                </td>	
+                            </tr>
+                            <tr>
+                                <td width="10%" id="CourseID">CourseID</td>
+                                <td width="70%" id="CourseDitails">CourseDitails</td>
+                                <td width="20%" id="Star">
+                                    <div class="review">
+                                        <span class="star">
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                        </span>
+                                        <span class="star-txt"></span>
+                                    </div>
+                                </td>	
+                            </tr>
+                            <tr>
+                                <td width="10%" id="CourseID">CourseID</td>
+                                <td width="70%" id="CourseDitails">CourseDitails</td>
+                                <td width="20%" id="Star">
+                                    <div class="review">
+                                        <span class="star">
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                        </span>
+                                        <span class="star-txt"></span>
+                                    </div>
+                                </td>	
+                            </tr>
+                        </table>
+                        <h2>Semenster 2</h2>
+                        <table width="100%" class="zebra review_table">
+                            <tr>
+                                <td width="10%" id="CourseID">CourseID</td>
+                                <td width="70%" id="CourseDitails">CourseDitails</td>
+                                <td width="20%" id="Star">
+                                    <div class="review">
+                                        <span class="star">
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                        </span>
+                                        <span class="star-txt"></span>
+                                    </div>
+                                </td>	
+                            </tr>
+                            <tr>
+                                <td width="10%" id="CourseID">CourseID</td>
+                                <td width="70%" id="CourseDitails">CourseDitails</td>
+                                <td width="20%" id="Star">
+                                    <div class="review">
+                                        <span class="star">
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                        </span>
+                                        <span class="star-txt"></span>
+                                    </div>
+                                </td>	
+                            </tr>
+                            <tr>
+                                <td width="10%" id="CourseID">CourseID</td>
+                                <td width="70%" id="CourseDitails">CourseDitails</td>
+                                <td width="20%" id="Star">
+                                    <div class="review">
+                                        <span class="star">
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                        </span>
+                                        <span class="star-txt"></span>
+                                    </div>
+                                </td>	
+                            </tr>
+                        </table>
+                        <h2>Semenster 3</h2>
+                        <table width="100%" class="zebra review_table">
+                            <tr>
+                                <td width="10%" id="CourseID">CourseID</td>
+                                <td width="70%" id="CourseDitails">CourseDitails</td>
+                                <td width="20%" id="Star">
+                                    <div class="review">
+                                        <span class="star">
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                        </span>
+                                        <span class="star-txt"></span>
+                                    </div>
+                                </td>	
+                            </tr>
+                            <tr>
+                                <td width="10%" id="CourseID">CourseID</td>
+                                <td width="70%" id="CourseDitails">CourseDitails</td>
+                                <td width="20%" id="Star">
+                                    <div class="review">
+                                        <span class="star">
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                        </span>
+                                        <span class="star-txt"></span>
+                                    </div>
+                                </td>	
+                            </tr>
+                            <tr>
+                                <td width="10%" id="CourseID">CourseID</td>
+                                <td width="70%" id="CourseDitails">CourseDitails</td>
+                                <td width="20%" id="Star">
+                                    <div class="review">
+                                        <span class="star">
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                            <b class="ct-star  ic-star-off"></b>
+                                        </span>
+                                        <span class="star-txt"></span>
+                                    </div>
+                                </td>	
+                            </tr>
+                        </table>
+                        <div class="button_part">
+                            <button type="submit" class="button">Submit</button>
+                            <a href="StudentProfile.html" class="button">Back</a>
+                        </div>
+                    </form>
+
+
+                    
+                    {/* <!-- contact information --> */}
+
+                </div>
+                {/* <!-- /row --> */}
+
+            </div>
+            {/* <!-- /container --> */}
+
+        </div>
+        {/* <!-- /Contact --> */}
+    </div>
+        
       </div>
     );
   }
