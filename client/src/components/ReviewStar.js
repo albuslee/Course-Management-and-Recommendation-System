d8a3d308faa11242a117312ae2b513fc295cd195
尹程 <yc@yinchengdeMacBook-Pro.local>
2018-10-05 11:52:29 +1000

    move to recommendation module

--- client/src/components/ReviewStar.js
+++ client/src/components/ReviewStar.js
@@ -6,31 +6,30 @@ class ReviewStar  extends  Component{
     constructor(props){
         super(props);
         this.state = {
-            CourseCode: this.props.CourseCode,
-            CourseStars: this.props.CourseStar,                                           // the star given by user.
+            review: this.props.index,
         }
     }
 
     componentDidMount(){
-        var scope = document.getElementsByClassName("review");      
-        for (var id = 0; id < scope.length; id++){
+        let scope = document.getElementsByClassName(this.state.review);       
+        for (let id = 0; id < scope.length; id++){
+            
             let aSpan=scope[id].getElementsByClassName("star")[0];
             let aStxt=scope[id].getElementsByClassName("star-txt")[0];
             let aBstar=aSpan.getElementsByTagName("b");
-            var arrBtxt=["Bad","Poor","Normal","Good","Perfect"];
-            console.log(this.state.CourseStars +'asdfsafsafasf'+ this.state.CourseCode);
-            let level = this.state.CourseStars;
-            aStxt.innerHTML=arrBtxt[level];
-                for(var i=0;i<=this.state.CourseStars;i++){
+            let arrBtxt=["Bad","Poor","Normal","Good","Perfect"];
+            const CourseStars = this.props['CourseStar'];     
+            aStxt.innerHTML=arrBtxt[CourseStars];
+                for(let i=0;i<CourseStars;i++){
                     aBstar[i].style.backgroundPosition="0 0";
-                }                
-            
+                }                   
         }
     }
 
     render(){
+
         return(
-            <div className="review">
+            <div className={this.state.review}>
                 <span className="star">
                     <b className="ct-star  ic-star-off"></b>
                     <b className="ct-star  ic-star-off"></b> 
