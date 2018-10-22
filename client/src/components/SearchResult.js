b97b1637106d933b7a6d85e73a4a839f75d7bcc0
Albus Lee <albuslee@gmail.com>
2018-10-22 21:05:07 +1100

    update code avg star

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -84,7 +84,7 @@ class SearchResult extends Component {
 		console.log('start', start_index, 'end', end_index);
 		console.log('state course', this.state.course_list)
 		return this.state.course_list.slice(start_index, end_index).map((course) => 
-			<CourseCard key={course._id} course_id={course._id} full_name={course.full_name} description={course.description} star={this.getRandom(3,5)}/>
+			<CourseCard key={course._id} course_id={course._id} full_name={course.full_name} description={course.description} star={course.code.avg_star}/>
 		)
 	}
 
