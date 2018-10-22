2a0db9cd74f34f7ae5a767e4c796e0ca9924b046
Albus Lee <Albus@ALMBP.local>
2018-10-22 21:05:07 +1100

    update code avg star

--- DataModel/plz_run_me_only.js
+++ DataModel/plz_run_me_only.js
@@ -84,38 +84,26 @@ const dropDB = new Promise((resolve, reject) => {
       })
       insertJson
       .then(() => {
-        const star = new Promise((resolve, reject) => {
-          let check_list = []
-          let all_course = ['1COMP9021', '1COMP9311', '1COMP9020', '2COMP9331', '2COMP9024', '2BINF6111', '2BINF9010', '2COMP9041', '2COMP9517', 
-          '2COMP9417', '2COMP9415', '2COMP9319', '2COMP9315', '2COMP9313', '2COMP9154', '2COMP9101', 
-          '2COMP6841', '2COMP6441', '2COMP4141']
-          all_course.map( code => {
-            enrollmentModel
-            .find({'course_list._id': code})
-            .exec(function(err, docs){
-              let star_list = []
-              let sum = 0
-              docs.filter(doc => {
-                doc.course_list.filter(course => {
-                  if(course._id === code){
-                    star_list.push(course.star)
-                  }
-                })
-              })
-              //console.log(code,star_list)
-              sum = getSum(star_list)
-              let mean = 0;
-              mean = (sum/star_list.length).toFixed(1);
+        const star_initial = new Promise((resolve, reject) => {
+          courseModel
+          .find({}, 'code')
+          .exec(function(err, docs){
+            let course_list = []
+            docs.forEach(doc => {
+              if(!course_list.includes(doc.code)){
+                course_list.push(doc.code)
+              }
+            });
+            console.log(course_list)
+            let check_list = []
+            course_list.map( course => {
               let newCode = new codeModel({
-                _id: code.slice(1,),
-                avg_star: mean
+                _id: course
               })
-              newCode
               .save()
               .then(() => {
                 check_list.push(0)
-                //console.log( code.slice(1,) + 'updated!')
-                if(check_list.length === all_course.length){
+                if(check_list.length === course_list.length){
                   resolve()
                 }
               })
@@ -123,13 +111,53 @@ const dropDB = new Promise((resolve, reject) => {
             })
           })
         })
-        star
+        star_initial
         .then(() => {
-          console.log('------------------------------------------')
-          console.log('All database has been setuped sucessfully! Enjoy!')
-          console.log('------------------------------------------')
-          mongoose.disconnect()
+          const star = new Promise((resolve, reject) => {
+            let check_list = []
+            let all_course = ['1COMP9021', '1COMP9311', '1COMP9020', '2COMP9331', '2COMP9024', '2BINF6111', '2BINF9010', '2COMP9041', '2COMP9517', 
+            '2COMP9417', '2COMP9415', '2COMP9319', '2COMP9315', '2COMP9313', '2COMP9154', '2COMP9101', 
+            '2COMP6841', '2COMP6441', '2COMP4141']
+            all_course.map( code => {
+              enrollmentModel
+              .find({'course_list._id': code})
+              .exec(function(err, docs){
+                let star_list = []
+                let sum = 0
+                docs.filter(doc => {
+                  doc.course_list.filter(course => {
+                    if(course._id === code){
+                      star_list.push(course.star)
+                    }
+                  })
+                })
+                //console.log(code,star_list)
+                sum = getSum(star_list)
+                let mean = 0;
+                mean = (sum/star_list.length).toFixed(1);
+                codeModel
+                .findByIdAndUpdate({'_id': code},
+                {'$set': {'avg_star': mean}},
+                {'new': true})
+                .exec(function(err, docs){
+                  if (err) {console.log(err)}
+                  check_list.push(0)
+                  if(check_list.length === all_course.length){
+                    resolve()
+                  }
+                })
+              })
+            })
+          })
+          star
+          .then(() => {
+            console.log('------------------------------------------')
+            console.log('All database has been setuped sucessfully! Enjoy!')
+            console.log('------------------------------------------')
+            mongoose.disconnect()
+          })
         })
+        
       }) 
   })
 
