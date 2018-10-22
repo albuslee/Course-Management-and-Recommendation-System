c52bf15c8cf86eef5f386eeb09fe53e7401f054f
Albus Lee <Albus@ALMBP.local>
2018-10-22 19:51:24 +1100

    update plzrunmeonly, calculate average star

--- DataModel/plz_run_me_only.js
+++ DataModel/plz_run_me_only.js
@@ -1,31 +1,135 @@
+// import external class
 let mongoose = require('mongoose');
-const url = "mongodb://localhost:27017/";
-
 let courseModel = require('./models/course.js');
 let userModel = require('./models/user.js');
 let enrollmentModel = require('./models/enrollment.js');
-let async = require('async');
+let codeModel = require('./models/code');
 
-let m = require('./user_course_insert.js');
+const url = "mongodb://localhost:27017/coursetest";
+const course_data = require('./data/opt_course_data2.json');
+const user_data = require('./data/user_data_up.json');
+const enrollment_data = require('./data/enrollment.json');
 
-const tasks = [
-	function insertData() {
-		mongoose.connect(url+'coursetest')
-		.then(
-			() => {
-				console.log('Database connect')
-			},
-			err => { console.log(err) }
-		)
-			
-		m.insert_json();
-	},
-	function disconnect(callback){mongoose.disconnect()}
-]
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
 
-async.series(tasks, (err, results) => {
-	if (err) {
-			console.log('dddddd');
-	}
-	console.log(results);
+function getSum(array) {
+  return array.reduce(function (r, a) {
+      return r + a;
+  }, 0);
+}
+
+const dropDB = new Promise((resolve, reject) => {
+  mongoose.connect(url)
+  .then(() => {
+    mongoose.connection.db.dropDatabase(function(err){
+      if(err){
+        reject(err)
+      }else{
+        resolve()
+      }
+    });
+  })
+  .catch(err => {
+    console.error('Database connection error')
+  })
 })
+  dropDB
+  .then(res => {
+      const insertJson = new Promise((resolve, reject) => {
+        let json_list = []
+        // insert json into mongoDB
+        courseModel.insertMany(course_data, function(err,result) {
+          if (err) {
+            // handle error
+            console.log(err);
+          } else {
+            // handle success
+            json_list.push(0)
+            if(json_list.length === 3){
+              resolve()
+            }
+            console.log("All courses are inserted successfully")
+          }
+        });
+
+        userModel.insertMany(user_data, function(err,result) {
+          if (err) {
+            // handle error
+            console.log(err);
+          } else {
+            // handle success
+            json_list.push(0)
+            if(json_list.length === 3){
+              resolve()
+            }
+            console.log("All users are inserted successfully")
+          }
+        });
+
+        enrollmentModel.insertMany(enrollment_data, function(err, result){
+          if (err) {
+            // handle error
+            console.log(err);
+          } else {
+            // handle success
+            json_list.push(0)
+            if(json_list.length === 3){
+              resolve()
+            }
+            console.log("All enrollment are inserted successfully")
+          }
+        })
+      })
+      insertJson
+      .then(() => {
+        const star = new Promise((resolve, reject) => {
+          let check_list = []
+          let all_course = ['1COMP9021', '1COMP9311', '1COMP9020', '2COMP9331', '2COMP9024', '2BINF6111', '2BINF9010', '2COMP9041', '2COMP9517', 
+          '2COMP9417', '2COMP9415', '2COMP9319', '2COMP9315', '2COMP9313', '2COMP9154', '2COMP9101', 
+          '2COMP6841', '2COMP6441', '2COMP4141']
+          all_course.map( code => {
+            enrollmentModel
+            .find({'course_list._id': code})
+            .exec(function(err, docs){
+              let star_list = []
+              let sum = 0
+              docs.filter(doc => {
+                doc.course_list.filter(course => {
+                  if(course._id === code){
+                    star_list.push(course.star)
+                  }
+                })
+              })
+              //console.log(code,star_list)
+              sum = getSum(star_list)
+              let mean = 0;
+              mean = (sum/star_list.length).toFixed(1);
+              let newCode = new codeModel({
+                _id: code.slice(1,),
+                avg_star: mean
+              })
+              newCode
+              .save()
+              .then(() => {
+                check_list.push(0)
+                //console.log( code.slice(1,) + 'updated!')
+                if(check_list.length === all_course.length){
+                  resolve()
+                }
+              })
+              .catch(err => console.log(err))
+            })
+          })
+        })
+        star
+        .then(() => {
+          console.log('------------------------------------------')
+          console.log('All database has been setuped sucessfully! Enjoy!')
+          console.log('------------------------------------------')
+          mongoose.disconnect()
+        })
+      }) 
+  })
+
